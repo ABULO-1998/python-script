20c229b51027438d80540d9990bfa74bbb98dfb5
ABULO-1998 <adongoabulo@gmail.com>
2025-03-13 22:41:32 +0300

    for loops

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+for x in range(5,10):
+    print(x)
+
+days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+
+for d in days:
+    print(d)
+
+for d in days:
+    if (d=='thursday'):break
+    print(d)    
+
+for d in days:
+    if(d=='thursday'):continue
+    print(d)  
