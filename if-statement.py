73382accbf9aefde65ebeb6b9c7aa8dd087fba21
ABULO-1998 <adongoabulo@gmail.com>
2025-03-15 15:35:17 +0300

    if statement

---
+++ if-statement.py
@@ -0,0 +1,9 @@
+Thomas_age = 2
+Age_at_kindergarten =5
+
+if Thomas_age < Age_at_kindergarten:
+    print("thomas should be in preschool")
+elif Thomas_age == Age_at_kindergarten:
+    print("enjoy kindergarten")
+else:
+    print("thomas should be in another class")  
